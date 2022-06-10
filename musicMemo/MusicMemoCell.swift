d500e2e077602716a11731132eb5f31e4e9fd762
Ryan.hong <ryan.hong@vipshop.com>
2022-06-10 10:08:12 +0800

    【Ryan.Hong】music memo 原始提交2

--- musicMemo/MusicMemoCell.swift
+++ musicMemo/MusicMemoCell.swift
@@ -10,15 +10,15 @@ import UIKit
 class MusicMemoCell: UITableViewCell {
     var album: Music! {
         didSet {
-            gameNameLabel.text = album.name
-            gameDetailLabel.text = album.detail
-            gameImageView.image = UIImage(named: album.pictureName)
+            albumNameLabel.text = album.name
+            albumDetailLabel.text = album.detail
+            albumImageView.image = UIImage(named: album.pictureName)
         }
     }
     
-    @IBOutlet weak var gameNameLabel: UILabel!
-    @IBOutlet weak var gameDetailLabel: UILabel!
-    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    @IBOutlet weak var albumDetailLabel: UILabel!
+    @IBOutlet weak var albumImageView: UIImageView!
     
     
     override func awakeFromNib() {
